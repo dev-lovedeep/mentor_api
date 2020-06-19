d5f77de5d7906ba8d0d16c01b9e216986838df3a
MojoAlpha <mohitp@mnnit.ac.in>
2020-06-19 17:10:18 +0530

    Signup Page Web View Completed

--- frontend/src/signup.js
+++ frontend/src/signup.js
@@ -4,8 +4,23 @@ class Signup extends Component {
     
     render(){
         return(
-            <div>
-                <p>This is a Signup Page</p>
+            <div className='login-grid-container'>
+                <div className='login-grid-item'>
+                    <h1 style={{fontSize:'4.2rem', marginBottom:'0.2em'}}>WELCOME <br /> TO MNNIT COMMUNITY</h1>
+                    <div className='signup-doodles'>
+                        <img style={{width:'30em', float:'left'}} src='/images/signup.png' />
+                    </div>
+                </div>
+                <div className='login-grid-item' style={{marginBottom: '2em'}}>
+                    <div className='login-form'>
+                        <h1>SIGNUP</h1>
+                        <form>
+                            <input type='text' placeholder='Enter Registration No.' /><br/>
+                            <div className='btn-outline' style={{marginTop:'3em', marginBottom:'2em'}}><button className='login-btn'>SIGNUP</button></div>
+                        </form>
+                        <h2 style={{marginTop:'3em'}}><a href='#'>LOGIN </a>HERE</h2>
+                    </div>
+                </div>
             </div>
         )
     }
